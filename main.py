31f5a92ba401062358b59e3f862d7bbdf4612963
Maciej Wolski <maciek.wol16@gmail.com>
2025-01-15 13:48:38 +0100

    Add files

---
+++ main.py
@@ -0,0 +1,258 @@
+import tkinter as tk
+from tkinter import messagebox
+import pyautogui
+import pytesseract
+import time
+import re
+import os
+import datetime
+import sys
+from pynput import mouse
+
+base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+tesseract_path = os.path.join(base_path, "Tesseract", "tesseract.exe")
+pytesseract.pytesseract.tesseract_cmd = tesseract_path
+
+
+class OverlayWindow(tk.Toplevel):
+    def __init__(self, master, x, y, w, h, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.overrideredirect(True)
+        self.attributes("-topmost", True)
+
+        self.config(bg="black")
+        self.attributes("-transparentcolor", "black")
+
+        self.geometry(f"{w}x{h}+{x}+{y}")
+
+        self.canvas = tk.Canvas(self, bg="black", highlightthickness=0, bd=0)
+        self.canvas.pack(fill="both", expand=True)
+
+        self.canvas.create_rectangle(
+            0, 0, w, h,
+            outline="red",
+            width=2,
+            fill=""
+        )
+
+    def close_overlay(self):
+        self.destroy()
+
+
+class StellarApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Stellar OCR Demo")
+
+        self.root.attributes("-topmost", True)
+
+        self.overlay_window = None
+
+        self.area = None
+        self.area_start = None
+        self.area_end = None
+        self.click_counter = 0
+        self.listener = None
+        self.running = False
+
+        self.log_file_path = self.create_log_file()
+        self.log_info(f"Application start - log saved in: {self.log_file_path}")
+
+        label_info = tk.Label(
+            root,
+            text=(
+                "STEPS:\n"
+                "1) Press define area 'Define area'\n"
+                "2) Press and hold LMB and drag it across\n"
+                "   to opposite corner.\n"
+                "   Pull off LMB to apply the AREA\n"
+                "3) Type in phrase 1 - for stellar option\n"
+                "   phrase 2 - for stellar force value \n"
+                "4) Press 'Start' and move ur mouse cursor to\n"
+                "   where 'Imprint' button is within the game"
+            )
+        )
+        label_info.pack(pady=5)
+
+        frame_phrases = tk.Frame(root)
+        frame_phrases.pack(pady=5)
+
+        label_phrase1 = tk.Label(frame_phrases, text="Phrase 1 (Penetration):")
+        label_phrase2 = tk.Label(frame_phrases, text="Phrase 2 (+15):")
+
+        self.entry_phrase1 = tk.Entry(frame_phrases, width=25)
+        self.entry_phrase2 = tk.Entry(frame_phrases, width=25)
+
+        label_phrase1.grid(row=0, column=0, padx=5, sticky="e")
+        self.entry_phrase1.grid(row=0, column=1, padx=5)
+        label_phrase2.grid(row=1, column=0, padx=5, sticky="e")
+        self.entry_phrase2.grid(row=1, column=1, padx=5)
+
+        self.btn_define_area = tk.Button(root, text="Define area", command=self.define_area)
+        self.btn_define_area.pack(pady=5)
+
+        self.btn_start = tk.Button(root, text="Start", command=self.start, state=tk.DISABLED)
+        self.btn_start.pack(pady=5)
+
+        self.btn_stop = tk.Button(root, text="Stop", command=self.stop, state=tk.DISABLED)
+        self.btn_stop.pack(pady=5)
+
+    def create_log_file(self):
+        """
+        Creates directory stellarlink_logs in user home directory and
+        returns path to new log file in this dir
+
+        File name format: YYYY-MM-DD_HH-MM-SS.txt
+        If file exists, then it adds _v2, _v3, etc.
+        """
+
+        home_dir = os.path.expanduser("~")
+        log_dir = os.path.join(home_dir, "stellarlink_logs")
+        os.makedirs(log_dir, exist_ok=True)
+
+        now_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        base_name = f"{now_str}.txt"
+        log_path = os.path.join(log_dir, base_name)
+
+        version = 2
+        while os.path.exists(log_path):
+            log_path = os.path.join(log_dir, f"{now_str}_v{version}.txt")
+            version += 1
+
+        return log_path
+
+    def log_info(self, message):
+        """
+        Adds line with timestamp to log file (self.log_file_path).
+        """
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        line = f"[{timestamp}] {message}\n"
+
+        with open(self.log_file_path, mode="a", encoding="utf-8") as f:
+            f.write(line)
+
+    def define_area(self):
+        messagebox.showinfo(
+            "Instruction",
+            "Go to the game screen, press LMB and hold it,\n"
+            "drag it across to the opposite corner."
+        )
+        self.area_start = None
+        self.area_end = None
+        self.click_counter = 0
+
+        if self.overlay_window is not None:
+            self.overlay_window.close_overlay()
+            self.overlay_window = None
+
+        def on_click(x, y, button, pressed):
+            if button == mouse.Button.left:
+                if pressed:
+                    if self.click_counter == 0:
+                        self.area_start = (x, y)
+                        self.click_counter += 1
+                else:
+                    if self.click_counter == 1:
+                        self.area_end = (x, y)
+                        self.calculate_area()
+                        self.listener.stop()
+                        self.click_counter += 1
+
+        self.listener = mouse.Listener(on_click=on_click)
+        self.listener.start()
+
+    def calculate_area(self):
+        if self.area_start and self.area_end:
+            x1, y1 = self.area_start
+            x2, y2 = self.area_end
+            left = min(x1, x2)
+            right = max(x1, x2)
+            top = min(y1, y2)
+            bottom = max(y1, y2)
+            width = right - left
+            height = bottom - top
+
+            if width <= 0 or height <= 0:
+                messagebox.showerror("Error", "Wrong area definition")
+                return
+
+            self.area = (left, top, width, height)
+            messagebox.showinfo("OK", f"Defined area: {self.area}")
+            self.log_info(f"Defined area: {self.area}")
+            self.btn_start.config(state=tk.NORMAL)
+
+            self.overlay_window = OverlayWindow(self.root, left, top, width, height)
+
+    def start(self):
+        if not self.area:
+            messagebox.showwarning("Missing area definition", "Fix area definition first!")
+            return
+
+        raw_phrase1 = self.entry_phrase1.get()
+        raw_phrase2 = self.entry_phrase2.get()
+        self.phrase1 = re.sub(r"\s+", "", raw_phrase1).lower()
+        self.phrase2 = re.sub(r"\s+", "", raw_phrase2).lower()
+
+        if not self.phrase1 or not self.phrase2:
+            messagebox.showwarning("Missing phrases", "You have to input both phrases")
+            return
+
+        self.log_info(f"Start OCR - phrase1={self.phrase1}, phrase2={self.phrase2}")
+
+        self.running = True
+        self.btn_stop.config(state=tk.NORMAL)
+        self.btn_start.config(state=tk.DISABLED)
+        self.root.after(100, self.loop_ocr)
+
+    def stop(self):
+        self.running = False
+        self.btn_stop.config(state=tk.DISABLED)
+        self.btn_start.config(state=tk.NORMAL)
+
+        self.log_info("Stop OCR - end of loop.")
+
+    def loop_ocr(self):
+        if not self.running:
+            return
+
+        x, y, w, h = self.area
+        screenshot = pyautogui.screenshot(region=(x, y, w, h))
+        text = pytesseract.image_to_string(screenshot)
+        text = re.sub(r"\s+", "", text).lower()
+        text = re.sub(r'([A-Za-z]+)4(\d)', r'\1+\2', text)
+
+        if "stellarforce4" in text:
+            text = text.replace("stellarforce4", "stellarforce+")
+
+        self.log_info(f"OCR text: {text}")
+
+        found_phrase1 = (self.phrase1 in text) if self.phrase1 else False
+        found_phrase2 = (self.phrase2 in text) if self.phrase2 else False
+
+        if found_phrase1 and found_phrase2:
+            messagebox.showinfo("Found it!", "Hopefully that's what you have been looking for")
+            self.log_info("Both found - finish")
+            self.stop()
+        elif found_phrase1 or found_phrase2:
+            self.log_info(f"One found: {text}")
+            pyautogui.click(button='left')
+            time.sleep(1)
+            pyautogui.click(button='left')
+            time.sleep(0.3)
+            self.root.after(100, self.loop_ocr)
+        else:
+            pyautogui.click(button='left')
+            time.sleep(1)
+            pyautogui.click(button='left')
+            time.sleep(0.3)
+            self.root.after(100, self.loop_ocr)
+
+
+def main():
+    root = tk.Tk()
+    app = StellarApp(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
